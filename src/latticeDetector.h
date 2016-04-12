0aec35f9a8b0d8e5d3bf0ecd14e0a04403f16476
promy22 <romy.profanter@hotmail.de>
2016-04-12 22:07:23 +0200

    added lattice boundary detection

--- src/latticeDetector.h
+++ src/latticeDetector.h
@@ -17,31 +17,39 @@ using namespace std;
 class LatticeDetector{
 
 public:
-	static const double VECTOR_DISTANCE=0;
+	static const double VECTOR_DISTANCE = 0;
+
+	static const double TRESHOLD1 = 0.1;
+
+	static const double TRESHOLD2 = 0.5;
 
 	vector<Vector3d> points;
 
 	vector<Vector3d> calculateCandidateVectors(bool naive);
 
-	void clusterCandidates(vector<Vector3d> &candidates, list<list<Vector3d> > &clusteredCandidates);
+	void clusterCandidates(vector<Vector3d> const &candidates, list<list<Vector3d> > &clusteredCandidates);
 
-	void combineCandidates(list<list<Vector3d> > &clusteredCandidates, vector<Vector3d> &finalCandidates, vector<int> &scores);
+	void combineCandidates(list<list<Vector3d> > const &clusteredCandidates, vector<Vector3d> &finalCandidates, vector<int> &scores);
 
-	Vector3d translationVector(Vector3d &point1, Vector3d &point2);
+	Vector3d translationVector(Vector3d const &point1, Vector3d const &point2);
 
-	bool vectorsAreSimilar(Vector3d vector1, Vector3d vector2);
+	bool vectorsAreSimilar(Vector3d const &vector1, Vector3d const &vector2);
 
-	vector<double> validateCandidateVectors(vector<Vector3d> candidateVectors);
+	vector<double> validateCandidateVectors(vector<Vector3d> const &candidateVectors);
 
-	double validateVector(Vector3d candidateVector);
+	double validateVector(Vector3d const &candidateVector);
 
-	double validInvalidRatio(Vector3d referencePoint, Vector3d candidateVector);
+	double validInvalidRatio(Vector3d const &referencePoint, Vector3d const &candidateVector);
 
-	bool isPointValid(Vector3d referencePoint, Vector3d candidateVector, int index);
+	bool isPointValid(Vector3d const &referencePoint, Vector3d const &pointToTest);
 
-	vector<Vector3d> projectPointsOnLine(Vector3d referencePoint, Vector3d candidateVector);
+	vector<Vector3d> projectPointsOnLine(Vector3d const &referencePoint, Vector3d const &candidateVector);
 
-	vector<int> getOutermostOnGridPointIndices(vector<Vector3d> projectedPoints, Vector3d referencePoint, Vector3d candidateVector);
+	vector<int> getOutermostOnGridPointIndices(vector<Vector3d> const &projectedPoints, Vector3d const &referencePoint, Vector3d const &candidateVector);
+
+	vector<Vector3d> calculateLatticeBoundary(Vector3d const &referencePoint, Vector3d const &latticeVector1, Vector3d const &latticeVector2);
+
+	bool validLine(Vector3d const &referencePoint, Vector3d const &anchorPoint, Vector3d const &directionVector, int length);
 
 };
 
