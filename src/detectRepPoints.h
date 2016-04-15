a0412f300766449cb326328b6407d877d4668dd4
ryenelith <ryenelith@Ryens-MacBook-Pro.local>
2016-04-15 18:12:02 +0200

    Updated reading grouping from file
    Updated comparisons to do counting, groups can be read from file too.
    Updated statistic only displaying stuff if recalculated.
    New format for output group file, makes reading groups possible.
    
    writing grouping to file is now part of the get groups command.

--- src/detectRepPoints.h
+++ src/detectRepPoints.h
@@ -53,11 +53,10 @@ private:
             // builds binary matrix pointsToTest with comparisons to execute
             int getPointsToTest();
 
-
         // Sift stuff
 
             // choice wheather to calculate sift from images or take from fiel
-            bool computeFromImages;
+            int computeOrRead;
 
             // struct to store 3D point information
             struct siftFeatures {
@@ -85,7 +84,6 @@ private:
             // function to computes all sift descriptors and fill siftFeatureVector and complete pointsToSift structs (with sift indexes)
             int get3DPointSiftRepresentations();
 
-
             // function to compute siftDescriptor of one image using openCV
             int computeSiftDescriptor(int imageIndex, Eigen::Vector2f pos, Eigen::VectorXf &outSingleFeatureVector);
 
@@ -96,6 +94,7 @@ private:
             vector<int> nextGroupIdx;                                   // next group index to assign for new groups (holds recycled groups)
             double tol_angle;                                           // decision criteria angle for repetitive points
             int countComparisons;                                       // count of comparisons executed to find groups
+            int comparisonsToDo;                                        // number of comparisons to execute
 
             // function to compare two 3D points based on their sift descriptors
             int compare3DPoints(int pointIdx1, int pointIdx2);
@@ -115,13 +114,16 @@ private:
             // getter method to get 3d location of given point index
             Eigen::Vector3d get3dFromPointIdx(int pointIndex);
 
+            // method to read groupOfPoints from file
+            int readGroupsFromFile();
+
             // vector with grouped 3d points (no recycled groups included)
             vector<vector<Eigen::Vector3d> > groupsOfPoints;
 
 
 public:
         // constructor
-        detectRepPoints(char** classArgv, bool computeFromImagesArg);
+        detectRepPoints(char** classArgv, int computeOrReadArg);
         // destructor
         ~detectRepPoints();
 
